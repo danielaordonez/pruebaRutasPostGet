ef0fcbb9ae7d1dfd71d0779a31f8735662848cf5
Unknown <dilcia_salgado1995@hotmail.com>
2017-05-30 18:56:29 -0600

    terminado

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,33 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/pruebaDaniela', function(req,res,next){
+  var datosGenerales = [
+                {"nombre":"daniela"},
+                {"apellido":"ordonez"},
+                {"direccion":"col Hato de Enmedio"},
+                {"telefono":"3369-0359"},
+                {"tipo de sangre":"O+"},
+                {"identidad":"0101-1995-01854"}
+              ];
+  res.json(datosGenerales);
+});
+
+router.get('/informacion', function(req,res,next){
+  res.render('Mensaje',{"txtmensaje":""});
+});
+
+var mensaje=[];
+
+router.post('/informacion', function(req,res,next){
+  console.log(req.body);
+  mensaje.push(req.body.txtmensaje.concat(" prueba"));
+  var msgs =mensaje;
+  var rtObject={};
+  rtObject.txtmensaje=req.body.txtmensaje;
+  rtObject.msg=msgs;
+  res.render('Mensaje', rtObject);
+});
+
 module.exports = router;
